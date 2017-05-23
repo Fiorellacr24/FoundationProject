340bcd8d26b1d213a401eecd6814731ad3b3247c
Fiorellacr24 <fiorella.0624@gmail.com>
2017-05-23 15:18:37 -0500

    add images

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
 $(document).foundation();
+<<<<<<< HEAD
+=======
+$(document).on('ready', function() {
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true
+  });
+});
+>>>>>>> sliders
